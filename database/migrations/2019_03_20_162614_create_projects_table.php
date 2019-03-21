8d3f8dd4dc88ca7d560b46015cf984fddcb5719a
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-03-20 18:08:15 -0600

    Cambiando a que pueda ser nulo la fecha de termino

--- database/migrations/2019_03_20_162614_create_projects_table.php
+++ database/migrations/2019_03_20_162614_create_projects_table.php
@@ -17,7 +17,7 @@ class CreateProjectsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->date('start_date');
-            $table->date('end_date');
+            $table->date('end_date')->nullable();
             $table->timestamps();
         });
     }
