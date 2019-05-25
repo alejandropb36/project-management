c120b095f33a26649e41ef1ecae3f7a3763a6402
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-05-24 22:24:47 -0500

    Creando usuario Admin con su Seed

---
+++ database/seeds/AdminUsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminUsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert(array(
+            'name' => 'Admin',
+            'surname' => 'Admin',
+            'nick' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin123'),
+            'created_at' => now(),
+        ));
+    }
+}
--- database/seeds/ProjectTableSeeder.php
+++ database/seeds/ProjectTableSeeder.php
@@ -11,20 +11,6 @@ class ProjectTableSeeder extends Seeder
      */
     public function run()
     {
-        /**
-         * Seeder para pobar tabla
-         * Ejemplo: Poblar ciudades, estados, pruebas, etc.
-         */
-        // $cant = 20;
-        // for($i=0; $i < $cant; $i++)
-        // {
-        //     DB::table('projects')->insert(array(
-        //         'name' => 'Proyecto ' . $i,
-        //         'description' => 'Descripcion de proyecto '. $i,
-        //         'status' => 'ACTIVO',
-        //         'start_date' => date('Y-m-d')
-        //     ));
-        // }
         $projects = factory(App\Project::class, 5)
             ->create()
             ->each(function ($project) {
